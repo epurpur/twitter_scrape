526392cb8ab73bc6fb2332dc2bf948baa6ce3ca8
Erich Purpur <epurpur@gmail.com>
2023-11-21 17:00:20 -0500

    test tweet gather

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -21,16 +21,12 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 
 
-
-
-#set up chromedriver
 MY_PATH = "./chromedriver"
 driver = webdriver.Chrome(executable_path=MY_PATH)
 wait = WebDriverWait(driver, 60)    # determines maximum wait time for an element to load
 
 
 #####STEP 1. LOGIN
-
 driver.get('https://twitter.com/i/flow/login')
 
 print('webpage accessed')
@@ -89,222 +85,169 @@ time.sleep(5)
 print('clicking search bar')
 search_box = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
 search_box.click()
-print('sending keys #BostonStrong')
-search_box.send_keys('#BostonStrong')
-#click enter button
+print('Sending search terms keys')
+search_box.send_keys('(#BostonStrong) lang:en until:2023-06-01 since:2023-03-01')
 print('press enter key')
 search_box.send_keys(Keys.RETURN)
 
+print('click latest button')
+latest_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Latest"]')))
+# Scroll into view
+print('scrolling into view')
+driver.execute_script("arguments[0].scrollIntoView(true);", latest_button)
+# Click on the element
+latest_button.click()
 
-print('Waiting for page to render')
-element = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41']")))
+#scrolling down page
+print('scrolling to bottom of page. This might take a while...')
 
-print('Extra wait time')
-time.sleep(15)
+##########START HERE. A FEW NOTES
+    #1. How to get content of all tweets after scrolling?
+    #2. How to scroll until I can't scroll any more? Or if I do a fixed number of scrolls, how to handle an error if it occurs
+scrolls = 3
 
-
-print('clicking advanced search box')
-max_attempts = 3
-for attempt in range(max_attempts):
-    try:
-        advanced_search_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@class="css-1qaijid r-bcqeeo r-qvutc0 r-poiln3" and @style="text-overflow: unset;"]')))
-        break  # Exit the loop if element is found
-    except TimeoutException:
-        if attempt == max_attempts - 1:
-            raise  # Raise the TimeoutException on the last attempt
-        else:
-            print(f"Attempt {attempt + 1} failed. Retrying...")
-
-advanced_search_box.click()
-
-time.sleep(10)
-
-
-print('clicking language box')
-language_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="SELECTOR_1"]')))
-language_box.click()
-
-print('Choosing English')
-select = Select(language_box)
-select.select_by_index(11)
-
-print('Clicking hashtags box')
-hashtags_box = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//input[@name="theseHashtags"]')))
-hashtags_box[0].click()  # presence_of_all_elements_located returns a list, so you need to access the first element
-hashtags_box[0].send_keys('#BostonStrong')
-
-# print("Moving on to Dates")
-# #########THIS WORKS
-# print('dates from month')
-# dates_from_month = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="SELECTOR_2"]')))
-# dates_from_month.click()
-# select_from_month = Select(dates_from_month)
-# select_from_month.select_by_index(3)   #select March?
-
-
-###################START HERE, MAKE THIS WORK? This is selecting the 'from day' dropdown
-print('dates from day')
-dates_from_day = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="SELECTOR_10"]')))
-print('1')
-dates_from_day.click()
-print('2')
-select_from_day = Select(dates_from_day)
-print('3')
-select_from_day.select_by_index(1)  #select 1st of the month
-print('4')
-
-
-
-########### doesn't work
-# print('dates from year')
-# dates_from_year = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#SELECTOR_11')))
-# dates_from_year.click()
-# select_from_year = Select(dates_from_year)
-# try:
-#     print('trying by index')
-#     select_from_year.select_by_index(1)  #select 2023
-# except Exception:
-#     print('trying by value')
-#     select_from_year.select_by_value('2023')
-
-
-
-
-
-
-
-# #######STEP . GATHER HTML
-# print('extracting info from tweets')
-# html = driver.page_source
-
-# soup = BeautifulSoup(html, 'lxml')
-
-# all_tweets = soup.find_all('div', class_='css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu')
-
-# #this holds all info gathered from each tweet on the page
-# final_results = []
-
-# for tweet in all_tweets:
+for scroll in range(scrolls):
+    print('scrolling')
+    current_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+    print("Current height of page: ", current_height)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(10)
     
-#     twitter_handle = ''
-#     username = ''
-#     date_of_tweet = []
-#     content = ''
-#     image_links = []
-#     video_links = []
+
+
+print('waiting for page to load')
+time.sleep(30)
+
+
+
+#######STEP 3. GATHER HTML
+print('extracting info from tweets')
+html = driver.page_source
+
+soup = BeautifulSoup(html, 'lxml')
+
+all_tweets = soup.find_all('div', class_='css-175oi2r r-16y2uox r-1wbh5a2 r-1ny4l3l')
+
+#this holds all info gathered from each tweet on the page
+final_results = []
+
+for tweet in all_tweets:
     
-#     # gets username, twitter handle, date of tweet
-#     first_nested_div = tweet.find('div', class_='css-1dbjc4n r-k4xj1c r-18u37iz r-1wtj0ep')
-#     if first_nested_div:
-#         # if these div tags are found
-#         for div in first_nested_div:
+    twitter_handle = ''
+    username = ''
+    date_of_tweet = []
+    content = ''
+    image_links = []
+    video_links = []
+    
+    # gets username, twitter handle, date of tweet
+    first_nested_div = tweet.find('div', class_='css-175oi2r r-k4xj1c r-18u37iz r-1wtj0ep')
+    if first_nested_div:
+        # if these div tags are found
+        for div in first_nested_div:
             
-#             #extract text content of the div
-#             div_text = div.get_text()
+            #extract text content of the div
+            div_text = div.get_text()
             
-#             #find and extract twitter handle
-#             for match in re.finditer(r'@(\w+)', div_text):
-#                 twitter_handle += match.group(0)
+            #find and extract twitter handle
+            for match in re.finditer(r'@(\w+)', div_text):
+                twitter_handle += match.group(0)
                 
-#             # Find and extract user's name
-#             for match in re.finditer(r'(?<!@)\b(?![A-Za-z]{3} \d{1,2}\b)\w+(?: \w+)+\b', div_text):
-#                 username += match.group(0)
+            # Find and extract user's name
+            for match in re.finditer(r'(?<!@)\b(?![A-Za-z]{3} \d{1,2}\b)\w+(?: \w+)+\b', div_text):
+                username += match.group(0)
                 
-#             # Find and extract date of tweet
-#             pattern = r'(?:[A-Z][a-z]{2} \d{1,2}|\d{1,2}h)'
-#             for match in re.finditer(pattern, div_text):
-#                 date_of_tweet = match.group(0)
+            # Find and extract date of tweet
+            pattern = r'(?:[A-Z][a-z]{2} \d{1,2}|\d{1,2}h)'
+            for match in re.finditer(pattern, div_text):
+                date_of_tweet = match.group(0)
                 
-#             for i, result in enumerate(date_of_tweet):
-#                 if re.match(pattern, result):
-#                     # Get the current date in the desired format "Nov 3"
-#                     current_date = datetime.now().strftime("%b %d")
-#                     current_date = current_date.replace(" 0", " ")  # Remove leading zero from the day
-#                     date_of_tweet[i] = current_date     
+            for i, result in enumerate(date_of_tweet):
+                if re.match(pattern, result):
+                    # Get the current date in the desired format "Nov 3"
+                    current_date = datetime.now().strftime("%b %d")
+                    current_date = current_date.replace(" 0", " ")  # Remove leading zero from the day
+                    date_of_tweet[i] = current_date     
 
-#     else:
-#         # if these div tags are not found
-#         twitter_handle = 'No twitter handle'
-#         username = 'No Name'
-#         date_of_tweet = 'No Date'
+    else:
+        # if these div tags are not found
+        twitter_handle = 'No twitter handle'
+        username = 'No Name'
+        date_of_tweet = 'No Date'
     
     
-#     # gets content of tweet including hashtags
-#     second_nested_div = tweet.find('div', class_='css-901oao css-cens5h r-18jsvk2 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0')
-#     if second_nested_div:
-#         # if these div tags are found
-#         tweet_contents = []
+    # gets content of tweet including hashtags
+    second_nested_div = tweet.find('div', class_='css-1rynq56 r-8akbws r-krxsd3 r-dnmrzs r-1udh08x r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-bnwqim')
+    if second_nested_div:
+        # if these div tags are found
+        tweet_contents = []
         
-#         for div in second_nested_div:
-#             for item in div:
-#                 # if contains span
-#                 if item.name == 'span':
-#                     tweet_contents.append(item.get_text())
-#                 # if contains a
-#                 elif item.name == 'a':
-#                     tweet_contents.append(item.get_text())
-#                 # if no other tag
-#                 else:
-#                     tweet_contents.append(item)
+        for div in second_nested_div:
+            for item in div:
+                # if contains span
+                if item.name == 'span':
+                    tweet_contents.append(item.get_text())
+                # if contains a
+                elif item.name == 'a':
+                    tweet_contents.append(item.get_text())
+                # if no other tag
+                else:
+                    tweet_contents.append(item)
                 
-#         #flatten tweet_contents into one long string
-#         tweet_contents = ''.join(tweet_contents)
-#         content = tweet_contents
+        #flatten tweet_contents into one long string
+        tweet_contents = ''.join(tweet_contents)
+        content = tweet_contents
         
         
-#     else:
-#         # if these div tags are not found
-#         tweet_contents = 'No Tweet Contents'
-#         content = tweet_contents
+    else:
+        # if these div tags are not found
+        tweet_contents = 'No Tweet Contents'
+        content = tweet_contents
         
         
-#     # gets content of other media. Can be 3 different things: an image, a retweet, a video
-#     # check for images first
-#     nested_imgs = tweet.find_all('img', class_="css-9pa8cd")
-#     if nested_imgs:
-#         for i in nested_imgs:
-#             # need to remove everything to the right of the "?" character
-#             src = i.get('src')
-#             src = src.split('?')[0]
-#             # check if last 4 characters of the string are .jpg aready
-#             if src[-4:] == '.jpg':
-#                 image_links.append(src)
-#             else:
-#                 src = src + '.jpg'    # add .jpg to end to complete string
-#                 image_links.append(src)
-#     else:
-#         image_links.append('No Images')
+    # gets content of other media. Can be 3 different things: an image, a retweet, a video
+    # check for images first
+    nested_imgs = tweet.find_all('img', class_="css-9pa8cd")
+    if nested_imgs:
+        for i in nested_imgs:
+            # need to remove everything to the right of the "?" character
+            src = i.get('src')
+            src = src.split('?')[0]
+            # check if last 4 characters of the string are .jpg aready
+            if src[-4:] == '.jpg':
+                image_links.append(src)
+            elif src[-5:] == '.jpeg':
+                image_links.append(src)
+            else:
+                src = src + '.jpg'    # add .jpg to end to complete string
+                image_links.append(src)
+    else:
+        image_links.append('No Images')
     
     
         
-#     # videos    
-#     nested_media = tweet.find_all('video')
-#     if nested_media:
-#         for i in nested_media:
-#             poster_img = i.get('poster')
-#             video_links.append(poster_img)
-#     else:
-#         video_links.append('No Video')
+    # videos    
+    nested_media = tweet.find_all('video')
+    if nested_media:
+        for i in nested_media:
+            poster_img = i.get('poster')
+            video_links.append(poster_img)
+    else:
+        video_links.append('No Video')
         
     
-#     # handle missing or empty data such as username, date of tweet, or content
-#     if len(username) == 0:
-#         username = 'No Name'
+    # handle missing or empty data such as username, date of tweet, or content
+    if len(username) == 0:
+        username = 'No Name'
         
-#     if len(date_of_tweet) == 0:
-#         date_of_tweet = 'No Date'
+    if len(date_of_tweet) == 0:
+        date_of_tweet = 'No Date'
     
-#     final_results.append([twitter_handle, username, date_of_tweet, content, image_links, video_links])
+    final_results.append([twitter_handle, username, date_of_tweet, content, image_links, video_links])
 
-#     print()
-#     print('~~~~~~~~~~~~~~~~~')
-#     print()
-
-
-
-
-
-
+    print()
+    print('~~~~~~~~~~~~~~~~~')
+    print()
 
     
     
