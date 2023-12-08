1944ca479fbc2325a0a0d6c1c344e2a519d51879
Erich Purpur <epurpur@gmail.com>
2023-12-08 10:57:10 -0500

    pushing 2013 data

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -88,7 +88,7 @@ search_box.click()
 print('Sending search terms keys')
 
 ##CHANGE THE DATES AND YEAR HERE
-search_box.send_keys('(#BostonStrong) lang:en until:2013-12-31 since:2013-01-01')
+search_box.send_keys('(#BostonStrong) lang:en until:2013-10-24 since:2013-01-01')
 print('press enter key')
 search_box.send_keys(Keys.RETURN)
 
